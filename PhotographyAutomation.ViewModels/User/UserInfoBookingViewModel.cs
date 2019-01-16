7cefb70e620c1e689c3c50abaf84882ddd847539
Ali (Home) <alisafaie@gmail.com>
2019-01-16 19:10:40 +0330

    اصلاح using های مربوط به ViewMode

--- PhotographyAutomation.ViewModels/User/UserInfoBookingViewModel.cs
+++ PhotographyAutomation.ViewModels/User/UserInfoBookingViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PhotographyAutomation.ViewModels.User
+﻿namespace PhotographyAutomation.ViewModels.User
 {
     public class UserInfoBookingViewModel
     {
